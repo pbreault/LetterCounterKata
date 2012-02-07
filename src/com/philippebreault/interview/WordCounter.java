7ee52a32075b0ee2f6499fb71c07b6912c7a7a0e
pbreault <pbreault@pyxis-tech.com>
2012-02-07 16:16:52 -0500

    First commit

---
+++ src/com/philippebreault/interview/WordCounter.java
@@ -0,0 +1,91 @@
+package com.philippebreault.interview;
+
+import java.util.HashMap;
+
+public class WordCounter {
+
+
+    private final HashMap<Character,Integer> mappings;
+
+    public WordCounter(String string) {
+        if(string == null) {
+            throw new NullPointerException("the string can not be null");
+        }
+
+        mappings = new HashMap<Character, Integer>();
+
+        for (int i = 0; i < string.length(); i++) {
+            char character = string.charAt(i);
+            addOccurrenceOf(character);
+        }
+    }
+
+    public void printAll(){
+        for (Character character : mappings.keySet()){
+            System.out.println(character + ": " + getCountOf(character));
+        }
+    }
+
+    public int getCountOf(Character character) {
+        Integer count = mappings.get(character);
+        if(count == null)
+            return 0;
+        else
+            return mappings.get(character);
+    }
+
+
+
+    private void addOccurrenceOf(char character){
+        if(!mappings.containsKey(character))
+            mappings.put(character, 0);
+
+        int previousCount = mappings.get(character);
+        mappings.put(character, previousCount+1);
+    }
+
+
+    
+    public static void main(String[] args){
+        WordCounter wordCounter = new WordCounter("The quick brown fox jumps over the lazy dog");
+        
+        System.out.println("a: " + wordCounter.getCountOf('a')); // a: 1
+        System.out.println("o: " + wordCounter.getCountOf('o')); // o: 4
+
+        wordCounter.printAll();
+
+        /*
+        Will return the following:
+            T: 1
+            f: 1
+            g: 1
+            d: 1
+             : 8
+            e: 3
+            b: 1
+            c: 1
+            a: 1
+            n: 1
+            o: 4
+            l: 1
+            m: 1
+            j: 1
+            k: 1
+            h: 2
+            i: 1
+            w: 1
+            v: 1
+            u: 2
+            t: 1
+            s: 1
+            r: 2
+            q: 1
+            p: 1
+            z: 1
+            y: 1
+            x: 1
+
+         */
+    }
+
+}
---
+++ test/com/philippebreault/interview/WordCounterTest.java
@@ -0,0 +1,32 @@
+package com.philippebreault.interview;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class WordCounterTest{
+
+    @Test
+    public void canCountNumbersOfLetters() throws Exception {
+        String string = "abcd abc";
+        
+        WordCounter wc = new WordCounter(string);
+
+        assertEquals(2, wc.getCountOf('a') );
+        assertEquals(1, wc.getCountOf('d') );
+        assertEquals(0, wc.getCountOf('z'));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void throwsExceptionIfNullStringIsPassed() {
+        //  should throw an exception
+        new WordCounter(null);
+    }
+
+    @Test
+    public void shouldWorkWithEmptyStrings() {
+        WordCounter wordCounter = new WordCounter("");
+
+        assertEquals(0, wordCounter.getCountOf('a'));
+    }
+}
